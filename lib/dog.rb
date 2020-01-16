6843c820edb6e832f792b54cf30d956c5d9ff794
Angelica  Thompson <i.thompson89@outlook.com>
2020-01-16 00:20:27 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,11 +7,11 @@ class Dog
 #   end 
   
 # end
- attr_reader :name
-  attr_accessor :mood
+# attr_reader :name
+#   attr_accessor :mood
 
-   def initialize(name, mood = "nervous")
-    @mood = mood
-    @name = name
-  end
-end 
+#   def initialize(name, mood = "nervous")
+#     @mood = mood
+#     @name = name
+#   end
+# end 
